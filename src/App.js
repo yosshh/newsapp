249e61444f6d3c50d8d581f3d943ad5cdb180a68
yash <yshukla2775@gmail.com>
2024-07-28 19:22:53 +0530

    Made some Changes in the application.

--- src/App.js
+++ src/App.js
@@ -33,12 +33,13 @@ export default class App extends Component {
         // onLoaderFinished={() => setProgress(0)}
       />
         <Routes>
+        <Route exact path="/home" element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='General' pageSize={10} country='in' category='General'/>}></Route>
+        <Route exact path="/" element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='General' pageSize={10} country='in' category='General'/>}></Route>
         <Route exact path="/Business" element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='business' pageSize={10} country='in' category='Business'/>}></Route>
         <Route exact path="/Sports"  element={<News setProgress={this.setProgress}  APIKey={this.APIKey} key='sports' pageSize={10} country='in' category='Sports'/>}></Route>
         <Route exact path="/Technology"  element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='technology' pageSize={10} country='in' category='Technology'/>}></Route>
         <Route exact path="/Entertainment"  element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='Entertainment' pageSize={10} country='in' category='Entertainment'/>}></Route>
         <Route exact path="/Health"  element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='Health' pageSize={10} country='in' category='Health'/>}></Route>
-        <Route exact path="/General"  element={<News setProgress={this.setProgress} APIKey={this.APIKey} key='General' pageSize={10} country='in' category='General'/>}></Route>
         <Route exact path="/Science"  element={<News setProgress={this.setProgress} APIKey={this.state.APIKey} key='Science' pageSize={10} country='in' category='Science'/>}></Route>
         </Routes>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,24 +6,10 @@ export default class navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="#">
-            Navbar
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="#">
+                <Link className="nav-link active " aria-current="page" to="/home">
                   Home
                 </Link>
               </li>
@@ -50,11 +36,6 @@ export default class navbar extends Component {
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/Health">
                   Health
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/General">
-                  General
                 </Link>
               </li>
               <li className="nav-item">
